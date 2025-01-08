13040fd113adfa499ae61764976419416b723b52
jcarbolynn <nda108252@gmail.com>
2025-01-08 14:43:07 -0500

    onChange to show search query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import MovieCard from "../components/movie_card"
+import {useState} from "react"
 
 function Home() {
+    // when state changes, component will re render
+    // name of state, function to change state, default value of state
+    const [searchQuery, setSearchQuery] = useState("");
     const movies = [
         {id: crypto.randomUUID(), title: "Harry Potter 1", release_date: "1998"},
         {id: crypto.randomUUID(), title: "Harry Potter 2", release_date: "2000"},
         {id: crypto.randomUUID(), title: "Harry Potter 3", release_date: "2002"}
     ]
 
+    const handleSearch = () => {
+        alert(searchQuery)
+    }
+
     return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    // when you set value to a state, have to set on change to
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
             <div className="movies-grid">
                 {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
             </div>
